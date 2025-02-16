17d2297ae814ca6f3d1bfa2cedfa1048e6a2d841
cstone45 <cstone45@usf.edu>
2025-02-16 05:48:37 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -5,3 +5,11 @@ function calculateSalary(baseSalary, bonus, taxRate) { //Declares the function c
 };
 calculateSalary(5000, 500, 0.1); //Generates an output of $5000.00
 calculateSalary(7000, 1000, 0.15); //Generates an output of 6950.00
+
+//Task 2: Function Expression
+const calculateDiscount = function(price, discountRate) { //Establishes calculate discount as a function
+    let finalPrice = price - (price * discountRate); //Creates the equation which will find the Final Price when the function is executed
+    console.log(`Final Price: $${finalPrice.toFixed(2)}`); //Logs the results of the Equation to the console as a template literal
+};
+calculateDiscount(100, 0.2); //Generates an output of $80.00
+calculateDiscount(250, 0.15); //Generates an output of $212.50
